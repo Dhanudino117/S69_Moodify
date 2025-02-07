2d0e5d833aa69269259e5b3e0630080f9e2bf1ca
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-02-07 10:18:33 +0530

    completed the assignment

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,5 +1,30 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(5000, (req, res) => console.log("This server is runnin on 5000"));
+require("dotenv").config();
+let a = false;
+const port = process.env.PORT;
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Mongodb is connected ");
+    a = true;
+  })
+  .catch((err) => console.log("Error while connecting: ", err));
+
+app.get("/", (req, res) => {
+  // res.send("Hello, welcome to my ASAP Project");
+  if (a) {
+    res.send("Connected to DataBase");
+  } else {
+    res.send(`Error while connecting `);
+  }
+});
+app.get("/ping", (req, res) => {
+  res.json({ message: "Pong" });
+});
+
+app.listen(port, (req, res) => {
+  console.log("Port is running on the LocalHost:", port);
+});
